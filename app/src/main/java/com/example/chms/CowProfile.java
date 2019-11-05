d3e0d5990d1058c26a83baf16ca13a81bc0c4a11
ranjit <barsaranjeet15@gmail.com>
2019-11-05 20:41:35 +0530

    commiting first time

--- app/src/main/java/com/example/chms/CowProfile.java
+++ app/src/main/java/com/example/chms/CowProfile.java
@@ -2,6 +2,8 @@ package com.example.chms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class CowProfile extends AppCompatActivity {
@@ -10,5 +12,16 @@ public class CowProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cow_profile);
+
+
+
+    }
+    public void createProfile(View v)
+    {
+
+        CHMSDatabase chms = new CHMSDatabase(this);
+        SQLiteDatabase db = chms.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("");
     }
 }
