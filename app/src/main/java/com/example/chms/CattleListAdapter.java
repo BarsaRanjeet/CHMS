0f890874ce28f8888e8e274d1cf8172a23a06599
mehulsinhzala96 <mehulsinhzala7434@gmail.com>
2019-11-19 16:39:43 +0530

    change ui

--- app/src/main/java/com/example/chms/CattleListAdapter.java
+++ app/src/main/java/com/example/chms/CattleListAdapter.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -46,9 +46,12 @@ public class CattleListAdapter extends ArrayAdapter
 
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inSampleSize = 8;
-        String rootDuirectory = Environment.getExternalStorageDirectory().getAbsolutePath();
-
-        final Bitmap bitmap = BitmapFactory.decodeFile(rootDuirectory+"/chms/"+cattleImages[position], options);
+        File file = new File(cattleImages[position]);
+        if(file.exists())
+            Toast.makeText(context, "Exist", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(context, "Not Exist", Toast.LENGTH_SHORT).show();
+        final Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
 
         cattleImage.setImageBitmap(bitmap);
         //cattleImage.setImageResource(cattleImages[position]);
