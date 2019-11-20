5e942ac80805f73d91266e78cf53bd513a2af089
ranjit <barsaranjeet15@gmail.com>
2019-11-20 14:01:53 +0530

    Merge branch 'Shahnavaz' of https://github.com/barsaranjeet15/CHMS into CHMS
    # Conflicts:
    #	app/src/main/res/layout/activity_owner_registration.xml

--- app/src/main/java/com/example/chms/CattleProfile.java
+++ app/src/main/java/com/example/chms/CattleProfile.java
@@ -16,7 +16,7 @@ import android.widget.Toast;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 public class CattleProfile extends AppCompatActivity {
-    TextView c_id,c_name,c_policy;
+    TextView c_id,c_name,c_policy,age,weight,noOfChild,motherId,fatherId,breed,status,nextHeat;
     ImageView c_image;
     CollapsingToolbarLayout c_title;
     @Override
@@ -34,6 +34,14 @@ public class CattleProfile extends AppCompatActivity {
         c_id = (TextView)findViewById(R.id.cattle_cuin);
         c_name = (TextView)findViewById(R.id.cattle_name);
         c_policy = (TextView)findViewById(R.id.cattle_policy);
+        age = (TextView)findViewById(R.id.cattle_age);
+        weight = (TextView)findViewById(R.id.cattle_weight);
+        noOfChild = (TextView)findViewById(R.id.cattle_child);
+        motherId = (TextView)findViewById(R.id.mother_id);
+        fatherId = (TextView)findViewById(R.id.father_id);
+        breed = (TextView)findViewById(R.id.cattle_breed);
+        status = (TextView)findViewById(R.id.cattle_status);
+        nextHeat = (TextView)findViewById(R.id.cattle_next_heat);
         c_title = (CollapsingToolbarLayout)findViewById(R.id.cattle_profile_title);
         c_image = (ImageView)findViewById(R.id.cattle_image);
         BitmapFactory.Options bitmapFactory = new BitmapFactory.Options();
@@ -43,10 +51,18 @@ public class CattleProfile extends AppCompatActivity {
         while(cursor.isAfterLast()==false) {
             bitmap = BitmapFactory.decodeFile(cursor.getString(cursor.getColumnIndex("cattle_image")));
             c_image.setImageBitmap(bitmap);
-            c_title.setTitle("cattle id : "+cursor.getString(cursor.getColumnIndex("cuin")));
-            c_id.setText("cattle id : "+cursor.getString(cursor.getColumnIndex("cuin")));
-            c_name.setText("cattle name : "+cursor.getString(cursor.getColumnIndex("cattle_name")));
-            c_policy.setText("cattle policy : "+cursor.getString(cursor.getColumnIndex("cattle_policy")));
+            c_title.setTitle("Cattle id : "+cursor.getString(cursor.getColumnIndex("cuin")));
+            c_id.setText("Cattle id : "+cursor.getString(cursor.getColumnIndex("cuin")));
+            c_name.setText("Cattle name : "+cursor.getString(cursor.getColumnIndex("cattle_name")));
+            c_policy.setText("Cattle policy : "+cursor.getString(cursor.getColumnIndex("cattle_policy")));
+            age.setText("cattle age : "+cursor.getString(cursor.getColumnIndex("age")));
+            weight.setText("Cattle weight : "+cursor.getString(cursor.getColumnIndex("weight")));
+            noOfChild.setText("No of child : "+cursor.getString(cursor.getColumnIndex("no_of_child")));
+            motherId.setText("Mother id : "+cursor.getString(cursor.getColumnIndex("mother_id")));
+            fatherId.setText("Father id : "+cursor.getString(cursor.getColumnIndex("father_id")));
+            breed.setText("Cattle breed : "+cursor.getString(cursor.getColumnIndex("breed")));
+            status.setText("Cattle status : "+cursor.getString(cursor.getColumnIndex("status")));
+            nextHeat.setText("Next heat on : "+cursor.getString(cursor.getColumnIndex("last_heat_on")));
 
              cursor.moveToNext();
         }
