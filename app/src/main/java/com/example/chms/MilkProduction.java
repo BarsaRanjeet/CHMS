4b65d3c908b0337945f1c7f3bc56c0b00fdf0497
ranjit <barsaranjeet15@gmail.com>
2019-11-23 02:01:43 +0530

    all of today

--- app/src/main/java/com/example/chms/MilkProduction.java
+++ app/src/main/java/com/example/chms/MilkProduction.java
@@ -1,5 +1,6 @@
 package com.example.chms;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,15 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class MilkProduction extends AppCompatActivity {
     private  static String[] dates = null;
     private static String[] milkQuantities = null;
+    private static String[] milkIds = null;
+    private static String[] milkTimes = null;
+
     private String cattleId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +26,56 @@ public class MilkProduction extends AppCompatActivity {
         cattleId = i.getStringExtra("cattleId");
 
         CHMSDatabase sqliteHelper = new CHMSDatabase(getApplicationContext());
-        SQLiteDatabase db = sqliteHelper.getReadableDatabase();
-        Cursor cursor = db.query("milk_detail",new String[]{},null,null,null,null,"milk_id DESC");
+        final SQLiteDatabase db = sqliteHelper.getReadableDatabase();
+        final Cursor cursor = db.query("milk_detail",new String[]{},null,null,null,null,"milk_id DESC");
         if(cursor.getCount()>0) {
 
             dates = new String[cursor.getCount()];
             milkQuantities = new String[cursor.getCount()];
+            milkIds = new String[cursor.getCount()];
+            milkTimes = new String[cursor.getCount()];
             cursor.moveToFirst();
             for (int j=0;cursor.isAfterLast()==false;j++)
             {
-                dates[j] = cursor.getString(2);
-                milkQuantities[j] = cursor.getString(4);
+                dates[j] = cursor.getString(cursor.getColumnIndex("date"));
+                milkQuantities[j] = cursor.getString(cursor.getColumnIndex("milk_qty"));
+                milkIds[j] = cursor.getString(cursor.getColumnIndex("milk_id"));
+                milkTimes[j] = cursor.getString(cursor.getColumnIndex("time"));
                 cursor.moveToNext();
             }
             ListView listV = (ListView) findViewById(R.id.milkDetail);
-            MilkProductionAdapter adapter = new MilkProductionAdapter(this, dates, milkQuantities);
+            MilkProductionAdapter adapter = new MilkProductionAdapter(this, milkIds, dates, milkQuantities,milkTimes);
             listV.setAdapter(adapter);
         }
+        final ListView milkList = (ListView)findViewById(R.id.milkDetail);
+        milkList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = (String)milkList.getItemAtPosition(i);
+                Cursor cursor1 = db.query("milk_detail",new String[]{},"milk_id=?",new String[]{item},null,null,null);
+                if(cursor1.getCount()>0)
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MilkProduction.this);
+                    builder.setTitle("Milk details");
+
+                    cursor1.moveToFirst();
+                    while (cursor1.isAfterLast()==false)
+                    {
+                        String[] milkDetails = {
+                                "Cattle id : "+cattleId,
+                                "Date : "+cursor1.getString(cursor1.getColumnIndex("date")),
+                                "Time : "+cursor1.getString(cursor1.getColumnIndex("time")),
+                                "Quantity : "+cursor1.getString(cursor1.getColumnIndex("milk_qty")),
+                                "Note : "+cursor1.getString(cursor1.getColumnIndex("note"))
+                        };
+                        builder.setItems(milkDetails,null);
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                        cursor1.moveToNext();
+                    }
+                }
+            }
+        });
     }
     public void newEntry(View v)
     {
--- app/src/main/java/com/example/chms/MilkProductionAdapter.java
+++ app/src/main/java/com/example/chms/MilkProductionAdapter.java
@@ -14,12 +14,17 @@ public class MilkProductionAdapter extends ArrayAdapter {
     private final Activity context;
     private final String[] dates;
     private final String[] milkQuantities;
+    private final String[] milkIds;
+    private final String[] milkTimes;
 
-    public MilkProductionAdapter(Activity context,String[] dates,String[] milkQuantities) {
-        super(context,R.layout.activity_milk_production_view,dates);
+
+    public MilkProductionAdapter(Activity context,String[] milkIds, String[] dates,String[] milkQuantities,String[] milkTimes) {
+        super(context,R.layout.activity_milk_production_view,milkIds);
         this.context = context;
         this.dates = dates;
         this.milkQuantities = milkQuantities;
+        this.milkIds = milkIds;
+        this.milkTimes = milkTimes;
     }
 
     @NonNull
@@ -27,10 +32,14 @@ public class MilkProductionAdapter extends ArrayAdapter {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View view = inflater.inflate(R.layout.activity_milk_production_view,null,true);
-        TextView milkDate = view.findViewById(R.id.milkDate);
-        TextView milkQuantity = view.findViewById(R.id.milkQuantity);
+        TextView milkDate = view.findViewById(R.id.milk_date);
+        TextView milkQuantity = view.findViewById(R.id.milk_qty);
+        TextView milkId = view.findViewById(R.id.milk_id);
+        TextView milkTime = view.findViewById(R.id.milk_time);
         milkDate.setText(dates[position]);
         milkQuantity.setText(milkQuantities[position]);
+        milkId.setText(milkIds[position]);
+        milkTime.setText(milkTimes[position]);
 
         return view;
     }
